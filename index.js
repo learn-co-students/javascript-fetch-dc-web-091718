c8b311343b3fa64b4c2f35cef8f0f0cc20e209e5
Jacqueline Chenault <adigitalnative@gmail.com>
2018-11-02 08:52:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const app = "I don't do much.";
+const token = '9afd6f25b65deecdbbe693b16296a3c2cdf3afd9'
+
+fetch('https://api.github.com/users/adigitalnative/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json))
