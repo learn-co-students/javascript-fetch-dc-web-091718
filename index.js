1a0fd2edf146abb2f3f7277970b852ea936808fb
DeCode2018 <decode2018@gmail.com>
2018-11-05 12:10:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // const app = "I don't do much.";
-const token = 'bd96451c846bbc16af227bd189d0f552c0652d65'
+const token = ''
 fetch('https://api.github.com/user/repos', {
   headers: {
     Authorization: `token ${token}`
